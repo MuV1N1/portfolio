8ae3caf7ddb99c749aa4b865cbf17575da39b805
MuV1N1 <marvin.kleiner1910@gmail.com>
2025-08-11 10:48:13 +0200

    feat: add projects collection migration and update rules
    - Created a new collection for projects with fields: id, name, description, sourceCodeUrl, and liveDemoUrl.
    - Added migration script to update collection rules for create, delete, list, update, and view.
    - Implemented login functionality with modal in the frontend.
    - Added initial structure for project creation script.
    - Prepared for fetching projects with a new script.

---
+++ src/scripts/login.ts
@@ -0,0 +1,50 @@
+import PocketBase from 'pocketbase';
+const pb = new PocketBase('http://127.0.0.1:8090');
+const loginButton = document.querySelector('.login-button') as HTMLLabelElement;
+const modal = document.getElementById('login-modal') as HTMLDivElement;
+const closeBtn = document.querySelector('.close-modal') as HTMLSpanElement;
+const loginForm = document.getElementById('login-form') as HTMLFormElement;
+const body = document.querySelector('body') as HTMLBodyElement;
+
+loginButton.addEventListener('click', () => {
+    modal.style.display = 'block';
+    body.style.overflow = 'hidden';
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+    body.style.overflow = 'auto';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+        body.style.overflow = 'auto';
+    }
+});
+
+loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const usernameInput = document.getElementById('username') as HTMLInputElement;
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    await pb.collection('users').authWithPassword(username, password)
+        .then((authData) => {
+            console.log('Login successful:', authData);
+            modal.style.display = 'none';
+            body.style.overflow = 'auto';
+            passwordInput.value = '';
+            localStorage.setItem('isAuthenticated', 'true');
+        })
+        .catch((error) => {
+            console.error('Login failed:', error);
+            alert('Login failed: ' + error.message);
+        });
+
+
+});
+
---
+++ src/scripts/projects/createProject.ts
@@ -0,0 +1,8 @@
+import PocketBase from 'pocketbase';
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+if(localStorage.getItem('isAuthenticated') === 'true') {
+
+}else {
+    
+}
